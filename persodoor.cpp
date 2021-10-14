bdbe962fdda0cdfcbd2ed92daa02cae0f28a9968
eiddeveloper <87811895+eiddeveloper@users.noreply.github.com>
2021-10-14 13:48:27 +0200

    log errors/30sec sleep

--- persodoor.cpp
+++ persodoor.cpp
@@ -132,7 +132,6 @@ int main(int argc, char** argv)
 
 {
     std::string PINstring = "";
-    std::strin
     std::string workflowoutputstring = "";
     //std::basic_string<TCHAR> textstr;
 
@@ -168,32 +167,8 @@ int main(int argc, char** argv)
 
     std::cout << "\nbuergerservice.org e.V. persodoor.\n\n" << std::endl;
 
-    // Check command line arguments.
-/*
-    if (argc != 2)
 
-    {
-
-        //outputstring = commandexec("ls -l");
-
-        std::cout << outputstring << std::endl;
-
-        std::cerr <<
-
-            "Usage: ./persodoor <PIN>\n" <<
-
-            "Example:\n" <<
-
-            "./persodoor 123456\n";
-
-        return EXIT_FAILURE;
-
-    }
-
-    PINstring = argv[1];
-*/
-
-    if(PINfile==NULL) perror ("Error opening file PIN.txt");
+    if(PINfile==NULL)perror ("Error opening file PIN.txt");
     else
     {
 
@@ -212,196 +187,108 @@ int main(int argc, char** argv)
 
     std::cout << "PIN from file : " << std::string(pinstr) << "Ende" << std::endl;
 
-
     PINstring=std::string(pinstr);
     PINstring.erase(std::remove_if(PINstring.begin(), PINstring.end(), isSpace()), PINstring.end());
+    if(PINstring=="")
+    {
+        PINstring="123456";
+    }
     //std::cout << "PIN= " << PINstring << std::endl;
 
 
     while(true)
     {
+        std::string outputstring ="";
         //std::this_thread::sleep_for(std::chrono::milliseconds(2000));
         std::cout << "---------------------------------------" << std::endl;
         std::cout << "starting startworkflow." << std::endl;
         writelog(log, "startworkflow.\n");
 
-        //start the workflow of the new workflowclass and show output
-
-        ////workflowoutputstring = wf.startworkflow(PINstring);
-
-        //workflowoutputstring="123456";
-
-        //fprintf(producedhashkey, workflowoutputstring.c_str());
-
-        //std::cout << "starting startworkflow." << std::endl;
-
-        //start the workflow of the new workflowclass and show output
-
         workflowoutputstring = wf.startworkflow(PINstring);
-
 
         outputstring = workflowoutputstring;
         if (outputstring == "e1")
         {
             std::cout << "ERROR - please check AusweisApp2, cardreader and Personalausweis!Exiting Plugin." << std::endl;
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE;
+            writelog(log, "ERROR e1 - please check AusweisApp2. cardreader. Personaausweis.\n");
         }
-
         else if (outputstring == "e2")
         {
             std::cout << "ERROR - please check your Personalausweis! Exiting Plugin." << std::endl;
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE;
+            writelog(log, "ERROR e2 - please check Personaausweis.\n");
         }
         else if (outputstring == "e3")
         {
             std::cout << "ERROR - please check your cardreader! Exiting Plugin." << std::endl;
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE;
+            writelog(log, "ERROR e3 - please check cardreader.\n");
         }
-
         else if (outputstring == "e4")
         {
 
             //std::cout << "ERROR - AusweisApp2-version less than 1.22.* please update! Exiting Plugin." << std::endl;
             std::cout << "ERROR - AusweisApp2-version less than 1.20.* please update! Exiting Plugin." << std::endl;
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE;
+            writelog(log, "ERROR e4 - AusweisApp2-version too low.\n");
         }
-
         else if (outputstring == "e5")
         {
             std::cout << "Warning - retryCounter of Perso <3, please start a selfauthentication direct with AusweisApp2! Exiting Plugin." << std::endl;
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE;
+            writelog(log, "ERROR e5 - retryCounter <3.\n");
         }
         else if (outputstring == "e7")
         {
             std::cout << "ERROR - no cardreader found! Exiting Plugin." << std::endl;
-            return EXIT_FAILURE;
+            //return EXIT_FAILURE;
+            writelog(log, "ERROR e7 - no cardreader found.\n");
         }
-
-        //workflowoutputstring="123\n";
-        //workflowoutputstring=workflowoutputstring+"\n";
-
-        std::cout << "workflowhash is: " << workflowoutputstring << std::endl;
-
-        std::string myhashkey="";
-        bool hashok=false;
-        workflowoutputstring.erase(std::remove_if(workflowoutputstring.begin(), workflowoutputstring.end(), isSpace()), workflowoutputstring.end());
-        if(hashkey==NULL) perror ("Error opening file hashkey.txt");
-        else
-        {
-           while(fgets(mystring, 100, hashkey)!=NULL)
-           {
-                std::cout << "hash from file : " << std::string(mystring) << std::endl;
-                myhashkey=std::string(mystring);
-                myhashkey.erase(std::remove_if(myhashkey.begin(), myhashkey.end(), isSpace()), myhashkey.end());
-
-                if(myhashkey == workflowoutputstring)
-                {
-                    hashok=true;
-                }
-            }
-         }
-
-        
-        if(hashok)
-        {
-           std::cout << "hashkeys are equal - unlocking door" << std::endl;
-           writelog(log, "unlocking door for hashkey "+workflowoutputstring+"\n");
-           outputstring = commandexec("python3 locking.py");
-           std::cout << outputstring << std::endl;
-        }
-        else
-        {
-           std::cout << "hashkeys are not equal - access denied" << std::endl;
-           writelog(log, "access denied for hashkey "+workflowoutputstring+"\n");
-
-        }
-
-
-
-
-
-        //fprintf(phashkey, workflowoutputstring.c_str());
-
-        /*
-
-        fp = popen("diff hashkey.txt producedhashkey.txt", "r");
-
-        //read output a line at a time
-
-        if(fgets(line, sizeof(line), fp)!=NULL)
-
-        {
-
-           std::cout << "diff not equal" << std::endl;
-
-        }
-
         else 
-
         {
+            //workflowoutputstring="123\n";
+            //workflowoutputstring=workflowoutputstring+"\n";
 
-           std::cout << "diff equal" << std::endl;
+            std::cout << "workflowhash is: " << workflowoutputstring << std::endl;
 
+            std::string myhashkey="";
+            bool hashok=false;
+            workflowoutputstring.erase(std::remove_if(workflowoutputstring.begin(), workflowoutputstring.end(), isSpace()), workflowoutputstring.end());
+            if(hashkey==NULL) perror ("Error opening file hashkey.txt");
+            else
+            {
+               while(fgets(mystring, 100, hashkey)!=NULL)
+               {
+                    std::cout << "hash from file : " << std::string(mystring) << std::endl;
+                    myhashkey=std::string(mystring);
+                    myhashkey.erase(std::remove_if(myhashkey.begin(), myhashkey.end(), isSpace()), myhashkey.end());
+
+                    if(myhashkey == workflowoutputstring)
+                    {
+                        hashok=true;
+                    }
+                }
+             }
+
+            
+            if(hashok)
+            {
+               std::cout << "hashkeys are equal - unlocking door" << std::endl;
+               writelog(log, "unlocking door for hashkey "+workflowoutputstring+"\n");
+               outputstring = commandexec("python3 locking.py");
+               std::cout << outputstring << std::endl;
+            }
+            else
+            {
+               std::cout << "hashkeys are not equal - access denied" << std::endl;
+               writelog(log, "access denied for hashkey "+workflowoutputstring+"\n");
+
+            }
         }
-
-        */
-
-
-
-
-
-        /*
-
-        std::cout << "---------------------------------------" << std::endl;
-
-        //read the output of the parsed jsondata
-
-        std::wcout << "parsed jsonresult as StyledString: \n" << strtowstr(wf.personalStyledString) << std::endl;
-
-        std::cout << "---------------------------------------" << std::endl;
-
-        //read the values of single datanames
-
-        std::wcout << "AcademicTitle: " << strtowstr(wf.AcademicTitle) << std::endl;
-
-        std::wcout << "ArtisticName: " << strtowstr(wf.ArtisticName) << std::endl;
-
-        std::wcout << "BirthName: " << strtowstr(wf.BirthName) << std::endl;
-
-        std::wcout << "DateOfBirth: " << strtowstr(wf.DateOfBirth) << std::endl;
-
-        std::wcout << "DocumentType: " << strtowstr(wf.DocumentType) << std::endl;
-
-        std::wcout << "FamilyNames: " << strtowstr(wf.FamilyNames) << std::endl;
-
-        std::wcout << "GivenNames: " << strtowstr(wf.GivenNames) << std::endl;
-
-        std::wcout << "IssuingState: " << strtowstr(wf.IssuingState) << std::endl;
-
-        std::wcout << "Nationality: " << strtowstr(wf.Nationality) << std::endl;
-
-        //personaldata PlaceOfBirth
-
-        std::wcout << "PlaceOfBirth: " << strtowstr(wf.PlaceOfBirth) << std::endl;
-
-        //personaldata PlaceOfResidence
-
-        std::wcout << "City: " << strtowstr(wf.City) << std::endl;
-
-        std::wcout << "Country: " << strtowstr(wf.Country) << std::endl;
-
-        std::wcout << "Street: " << strtowstr(wf.Street) << std::endl;
-
-        std::wcout << "ZipCode: " << strtowstr(wf.ZipCode) << std::endl;
-
-        std::cout << "---------------------------------------\n" << std::endl;
-
-        std::wcout << "keyhash of workflow: \n" << strtowstr(outputstring) << "\n\n" << std::endl;
-
-        std::cout << "---------------------------------------" << std::endl;
-
-        */
-
+        std::cout << "waiting 30 seconds..." << std::endl;
+        outputstring = commandexec("sleep 30");
+        
     }
 
 

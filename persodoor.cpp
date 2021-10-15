08e7dc723733c7c42ad9b64410d1ac7261c46650
eiddeveloper <87811895+eiddeveloper@users.noreply.github.com>
2021-10-15 10:36:13 +0200

    searchbug

--- persodoor.cpp
+++ persodoor.cpp
@@ -1,119 +1,68 @@
 // workflowClient.cpp : Test of workflowLibrary
-
 // Copyright (C) 2021 buergerservice.org e.V. <KeePerso@buergerservice.org>
-
 // Distributed under the Boost Software License, Version 1.0.
-
 // (See accompanying file LICENSE_1_0.txt or copy at
-
 // https://www.boost.org/LICENSE_1_0.txt)
 
 
-
-
-
 #include <iostream>
-
 #include <cstdio>
-
 #include <memory>
-
 #include <stdexcept>
-
 #include <string>
-
 #include <array>
-
 #include <chrono>
-
 #include <thread>
-
 #include <iostream>
-
 #include <time.h>
-
 #include <algorithm>
 
 //for workflowLibraryLinux.a
-
 #include "workflowLibrary.h"
 
 
 
-
-
-
-
-
-
 std::wstring strtowstr(const std::string& s)
-
 {
-
     std::wstring wsTmp(s.begin(), s.end());
-
     //ws = wsTmp;
-
     return wsTmp;
-
 }
 
 
-
-
-
 std::string commandexec(const char* cmd) {
-
     std::array<char, 128> buffer;
-
     std::string result;
-
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
 
     if (!pipe) 
     {
-
         throw std::runtime_error("popen() failed!");
-
     }
-
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) 
     {
-
         result += buffer.data();
-
     }
 
     return result;
-
 }
 
 
 
 void writelog(FILE *l, std::string s){
-
     time_t rawtime;
-
     struct tm * timeinfo;
-
     std::string c;
-
     char buffer[80];
-
     time(&rawtime);
-
     timeinfo=localtime(&rawtime);
 
     //printf("local time and date: %s", ctime(&rawtime));
 
     strftime(buffer,sizeof(buffer),"%d-%m-%Y %H:%M:%S",timeinfo);
-
     std::string str(buffer);
-
     c=str +": " + s;  
-
     fprintf(l, c.c_str());
-
 }
 
 
@@ -129,7 +78,6 @@ struct isSpace
 
 
 int main(int argc, char** argv)
-
 {
     std::string PINstring = "";
     std::string workflowoutputstring = "";
@@ -144,48 +92,41 @@ int main(int argc, char** argv)
     char line[1035];
     char mystring[100];
     char pinstr[100];
-
-
+    std::string myhashkey="";
     time_t rawtime;
     struct tm * timeinfo;
 
 
     log = fopen("persodoor.log", "a+");
 
-    hashkey = fopen("hashkey.txt", "r");
+    //hashkey = fopen("hashkey.txt", "r");
     PINfile=fopen("PIN.txt", "r");
 
     writelog(log, "persodoor started.\n"); 
 
-    
 
     //instantiate a new workflowclass
 
     workflowLibrary::workflow wf;
     printf("local time and date: %s", ctime(&rawtime));
 
-
     std::cout << "\nbuergerservice.org e.V. persodoor.\n\n" << std::endl;
-
 
     if(PINfile==NULL)perror ("Error opening file PIN.txt");
     else
     {
-
        if(fgets(pinstr, 100, PINfile)!=NULL)
        {
           //puts(mystring);
           std::cout << "PIN.txt read" << std::endl;
-
        }
-
        else
        {
           std::cout << "cant read PIN.txt" << std::endl;
        }
     }
 
-    std::cout << "PIN from file : " << std::string(pinstr) << "Ende" << std::endl;
+    std::cout << "PIN from file : " << std::string(pinstr) << std::endl;
 
     PINstring=std::string(pinstr);
     PINstring.erase(std::remove_if(PINstring.begin(), PINstring.end(), isSpace()), PINstring.end());
@@ -194,7 +135,18 @@ int main(int argc, char** argv)
         PINstring="123456";
     }
     //std::cout << "PIN= " << PINstring << std::endl;
-
+    /*
+    if(hashkey==NULL) perror ("Error opening file hashkey.txt");
+    else
+    {
+        while(fgets(mystring, 100, hashkey)!=NULL)
+        {
+            std::cout << "hashkey(s) from file : " << std::string(mystring) << std::endl;
+            myhashkey=std::string(mystring);
+            myhashkey.erase(std::remove_if(myhashkey.begin(), myhashkey.end(), isSpace()), myhashkey.end());
+        }
+    }
+    */
 
     while(true)
     {
@@ -252,9 +204,10 @@ int main(int argc, char** argv)
 
             std::cout << "workflowhash is: " << workflowoutputstring << std::endl;
 
-            std::string myhashkey="";
+            //std::string myhashkey="";
             bool hashok=false;
             workflowoutputstring.erase(std::remove_if(workflowoutputstring.begin(), workflowoutputstring.end(), isSpace()), workflowoutputstring.end());
+            hashkey = fopen("hashkey.txt", "r");
             if(hashkey==NULL) perror ("Error opening file hashkey.txt");
             else
             {
@@ -269,8 +222,8 @@ int main(int argc, char** argv)
                         hashok=true;
                     }
                 }
-             }
-
+            }
+            fclose(hashkey);
             
             if(hashok)
             {
@@ -286,15 +239,12 @@ int main(int argc, char** argv)
 
             }
         }
-        std::cout << "waiting 30 seconds..." << std::endl;
-        outputstring = commandexec("sleep 30");
-        
+        std::cout << "waiting 15 seconds..." << std::endl;
+        outputstring = commandexec("sleep 15");
     }
 
-
-    fclose(hashkey);
+    //fclose(hashkey);
     fclose(PINfile);
-
     fclose(log);
 
 }

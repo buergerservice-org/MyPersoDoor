9ad91c88746866674914a0ecffe1130d10a913d3
eiddeveloper <87811895+eiddeveloper@users.noreply.github.com>
2021-10-20 14:10:11 +0200

    Update persodoor.cpp

--- persodoor.cpp
+++ persodoor.cpp
@@ -1,9 +1,5 @@
-// workflowClient.cpp : Test of workflowLibrary
+// persodoor.cpp
 // Copyright (C) 2021 buergerservice.org e.V. <KeePerso@buergerservice.org>
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-// https://www.boost.org/LICENSE_1_0.txt)
-
 
 #include <iostream>
 #include <cstdio>
